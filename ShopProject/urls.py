66ce502f8110ea82183b683f948f7a3db70d0379
KIWONY <kien1004@naver.com>
2022-04-28 18:15:38 +0900

    add urls to  ShopProject/urls.py

--- ShopProject/urls.py
+++ ShopProject/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.urls import reverse_lazy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('shops/',include('shopapp.urls')),
+    path('accounts/',include("accountapp.urls")),
+
 ]
+
+
+
+
