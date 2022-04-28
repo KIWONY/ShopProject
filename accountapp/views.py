84d695d8e0fa883fe08f1b2e4939021a73afbcad
KIWONY <kien1004@naver.com>
2022-04-28 21:56:30 +0900

    api view using serializer

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,27 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.parsers import JSONParser
+
+from accountapp.models import Comment
+from accountapp.serializers import CommentSerializer
+
+
+def comment_list(request):
+    if request.method == "GET":
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return JsonResponse(serializer.data, safe= False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parser(request)
+        serializer = CommentSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            # https://www.django-rest-framework.org/api-guide/status-codes/#successful-2xx
+            return JsonResponse(serializer.data, status=201)
+
+        return JsonResponse(serializer.errors, status=400)
