7acad4d5d2af14191cac77c8ed81344303771c65
KIWONY <kien1004@naver.com>
2022-04-30 00:50:31 +0900

    use drf api_view decorator instead of csrf_exempt

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,32 +4,34 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 
 from accountapp.models import Comment
 from accountapp.serializers import CommentSerializer
 
 # https://www.django-rest-framework.org/tutorial/1-serialization/#writing-regular-django-views-using-our-serializer
-@csrf_exempt
+@api_view(["GET","POST"])
 def comment_list(request):
     if request.method == "GET":
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many=True)
-        return JsonResponse(serializer.data, safe= False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         data = JSONParser().parser(request)
-        serializer = CommentSerializer(data=data)
+        serializer = CommentSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
 
             # https://www.django-rest-framework.org/api-guide/status-codes/#successful-2xx
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+@api_view(["GET","PUT","DELETE"])
 def comment_detail(request,pk):
     try:
         comment = Comment.objects.get(pk=pk)
@@ -38,16 +40,16 @@ def comment_detail(request,pk):
 
     if request.method == "GET":
         serializer = CommentSerializer(comment)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == "PUT":
         data = JSONParser().parse(request)
-        serializer = CommentSerializer(comment, data = data)
+        serializer = CommentSerializer(comment, data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         comment.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
